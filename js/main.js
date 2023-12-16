233e1703e841ec3b8ecff752929d350e65e3bea7
amr rashad <khaledamr400@gmail.com>
2023-12-16 10:58:19 +0200

    add and show product functionality

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var siteName = document.getElementById("siteName")
+var siteUrl = document.getElementById("siteUrl")
+var submitBtn = document.querySelector("#formSubmit")
+
+var bookmarkList = []
+
+if(localStorage.getItem("bookmarks") != null){
+    bookmarkList = JSON.parse(localStorage.getItem("bookmarks"))
+    showBookmark()
+}
+
+submitBtn.addEventListener("click", addBookmark)
+
+function addBookmark() {
+    var bookmark = {
+        name: siteName.value,
+        url: siteUrl.value
+    }
+    bookmarkList.push(bookmark)
+    siteName.value = ""
+    siteUrl.value = ""
+    showBookmark()
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarkList))
+}
+
+function showBookmark() {
+    var temp = ""
+    for (var i = 0; i < bookmarkList.length; i++) {
+        temp += `  
+    <tr>
+        <td>`+i+`</td>
+        <td>`+bookmarkList[i].name+`</td>
+        <td><a href=""><button class="btn btn-outline-danger">Visit</button></a></td>
+        <td><a href=""><button class="btn btn-outline-danger">Delete</button></a></td>
+    </tr>`
+    }
+    document.getElementById("myContent").innerHTML = temp
+}
